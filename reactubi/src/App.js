fecf2a02bba70811c273e821dd60a5d5d59e429a
MrSallivan <nik.sallivan@gmail.com>
2023-04-14 23:46:22 +1000

    post list

--- reactubi/src/App.js
+++ reactubi/src/App.js
@@ -1,20 +1,32 @@
 import React, { useState } from 'react'
+import ClassCounter from './components/ClassCounter'
 import Counter from './components/Counter'
-import ClassCounter from './components/ClassCounter';
+import './styles/App.css'
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
 
 function App() {
-
-	const [value, setValue] = useState('Text in imput')
-
-
+	const [posts, setPosts] = useState([
+		{ id: 1, title: 'JavaScript', body: 'JavaScript - this is programming Language' },
+		{ id: 2, title: 'JavaScript', body: 'JavaScript - this is programming Language' },
+		{ id: 3, title: 'JavaScript', body: 'JavaScript - this is programming Language' },
+		{ id: 4, title: 'JavaScript', body: 'JavaScript - this is programming Language' },
+		{ id: 5, title: 'JavaScript', body: 'JavaScript - this is programming Language' },
+	])
+	const [posts2, setPosts2] = useState([
+		{ id: 1, title: 'Python', body: 'Python - this is programming Language' },
+		{ id: 2, title: 'Python', body: 'Python - this is programming Language' },
+		{ id: 3, title: 'Python', body: 'Python - this is programming Language' },
+		{ id: 4, title: 'Python', body: 'Python - this is programming Language' },
+		{ id: 5, title: 'Python', body: 'Python - this is programming Language' },
+	])
 
 	return (
-		<div className="App">
-			<ClassCounter />
-			<Counter />
-
+		<div className='App'>
+			<PostList posts={posts} title={'Post List from props'}/>
+			<PostList posts={posts2} title={'Post List from Python'}/>
 		</div>
-	);
+	)
 }
 
-export default App;
+export default App
--- reactubi/src/components/Counter.jsx
+++ reactubi/src/components/Counter.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react'
 
-const Counter = ()=>{
+function Counter() {
 	const [count, setCount] = useState(0)
-	
+
 	function increment() {
 		setCount(count + 1)
 	}
 	function decrement() {
-		setCount(count - 1)
 
+		setCount(count - 1)
 	}
+
 	return (
 		<div>
 			<h1>{count}</h1>
@@ -18,5 +19,4 @@ const Counter = ()=>{
 		</div>
 	)
 }
-
 export default Counter
