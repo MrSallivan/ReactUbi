ebc7b6a2218a26a925809c53fe803e0c9896f469
MrSallivan <nik.sallivan@gmail.com>
2023-04-23 07:50:34 +1000

    form

---
+++ reactubi/src/components/UI/button/MyButton.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import classes from './MyButton.module.css'
+
+
+const MyButton = () => {
+
+	return (
+		<button className={classes.myBtn}>Create post</button>
+
+	)
+}
+export default MyButton
